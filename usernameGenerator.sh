d2985d0039b2346886c7e936355189ef38620105
21y4d <26864473+21y4d@users.noreply.github.com>
2020-09-11 10:42:21 +0300

    Update usernameGenerator.sh

--- usernameGenerator.sh
+++ usernameGenerator.sh
@@ -9,22 +9,22 @@ fi
 firstname=$(echo $1 | tr '[:upper:]' '[:lower:]')
 lastname=$(echo $2 | tr '[:upper:]' '[:lower:]')
 
-echo "${firstname}" >"$firstname".txt
-echo "${lastname}" >>"$firstname".txt
+echo "${firstname}" >"$firstname"_usernames.txt
+echo "${lastname}" >>"$firstname"_usernames.txt
 
 #firstname last name
-echo "${firstname}${lastname}" >>"$firstname".txt
-echo "${firstname}.${lastname}" >>"$firstname".txt
-echo "${firstname:0:1}.${lastname}" >>"$firstname".txt
-echo "${firstname:0:1}${lastname}" >>"$firstname".txt
-echo "${firstname}${lastname:0:1}" >>"$firstname".txt
+echo "${firstname}${lastname}" >>"$firstname"_usernames.txt
+echo "${firstname}.${lastname}" >>"$firstname"_usernames.txt
+echo "${firstname:0:1}.${lastname}" >>"$firstname"_usernames.txt
+echo "${firstname:0:1}${lastname}" >>"$firstname"_usernames.txt
+echo "${firstname}${lastname:0:1}" >>"$firstname"_usernames.txt
 
 #lastname firstname
-echo "${lastname}${firstname}" >>"$firstname".txt
-echo "${lastname}.${firstname}" >>"$firstname".txt
-echo "${lastname:0:1}.${firstname}" >>"$firstname".txt
-echo "${lastname:0:1}${firstname}" >>"$firstname".txt
-echo "${lastname}${firstname:0:1}" >>"$firstname".txt
+echo "${lastname}${firstname}" >>"$firstname"_usernames.txt
+echo "${lastname}.${firstname}" >>"$firstname"_usernames.txt
+echo "${lastname:0:1}.${firstname}" >>"$firstname"_usernames.txt
+echo "${lastname:0:1}${firstname}" >>"$firstname"_usernames.txt
+echo "${lastname}${firstname:0:1}" >>"$firstname"_usernames.txt
 
-echo -e "\033[0;32mWordlist saved as ${firstname}.txt"
+echo -e "\033[0;32mWordlist saved as ${firstname}_usernames.txt"
 echo -e "\033[0m"
